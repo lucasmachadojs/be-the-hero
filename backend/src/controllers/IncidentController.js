3af59d5bd60b4c99b9d78630a261333188d9ff0a
Lucas Machado <lucasgms2002@gmail.com>
2020-03-29 13:49:49 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,14 +2,14 @@ const connection = require('../database/connection')
 
 module.exports = {
   async index(request, response) {
-    const { page = 1 } = request.body;
+    const { page = 1 } = request.query;
 
     const [count] = await connection('incidents').count();
 
     const incidents = await connection('incidents')
     .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-    .limit(5)
-    .offset((page -1) * 5)
+	.limit(5)
+    .offset(5 * (page - 1))
     .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
 
     response.header('X-Total-Count', count['count(*)']);
